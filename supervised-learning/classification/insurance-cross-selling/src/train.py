a3341fc1daac1585e2c8dfbb9eead8e25462a000
Jorge Gonzalez <jorgeg@mill5.com>
2024-10-11 15:56:31 -0500

    Final experiment code with conclusions

--- supervised-learning/classification/insurance-cross-selling/src/train.py
+++ supervised-learning/classification/insurance-cross-selling/src/train.py
@@ -32,8 +32,8 @@ def main(args):
     train_df = read_data(args.train_data)
     test_df = read_data(args.test_data)
     
-    # Count the number of row grouped by the target variable Response
-    print(train_df.groupby('Response').size())
+    print("Balancing data based on labels...")
+    train_df = balance_data(train_df)
     
     categorical_columns = ["Gender", "Vehicle_Age", "Vehicle_Damage"]
     numerical_columns = ["Age", "Annual_Premium","Policy_Sales_Channel", "Vintage", "Region_Code"]
@@ -59,27 +59,30 @@ def main(args):
     y_train = train_df['Response']
     
     # Split the data for validation
-    #X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.7, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.7, random_state=42)
 
     # Train using logistic regression
-    # regularization_rate = 0.1
-    # solver = 'liblinear'
-    # with mlflow.start_run(run_name=get_friendly_run_id("LogisticRegression_sag")):
-    #     model = train_logistic_regression_model(X_train, y_train, regularization_rate, solver)
-    #     evaluate_model(model, X_test, y_test)
+    regularization_rate = 0.1
+    solver = 'liblinear'
+    with mlflow.start_run(run_name=get_friendly_run_id("LogisticRegression_sag")):
+        model = train_logistic_regression_model(X_train, y_train, regularization_rate, solver)
+        evaluate_model(model, X_test, y_test)
     
     # Train using Random Forest
-    # n_estimators = 100
-    # criterion = 'gini'
-    # with mlflow.start_run(run_name=get_friendly_run_id("RandomForestClassifier")):
-    #     model = train_random_forest_model(X_train, y_train, n_estimators, criterion)
-    #     evaluate_model(model, X_test, y_test)
+    n_estimators = 100
+    criterion = 'gini'
+    with mlflow.start_run(run_name=get_friendly_run_id("RandomForestClassifier")):
+        model = train_random_forest_model(X_train, y_train, n_estimators, criterion)
+        evaluate_model(model, X_test, y_test)
         
     # Train using Neural Network
     # Define the K-fold Cross Validator
     num_folds = 5
     kfold = KFold(n_splits=num_folds, shuffle=True)
     fold_no = 1
+    
+    X_train = train_df.drop('Response', axis=1)
+    y_train = train_df['Response']
     
     # for train, test in kfold.split(X_train, y_train):
     #     X_train_kf, X_test_kf = X_train.iloc[train], X_train.iloc[test]
@@ -93,16 +96,7 @@ def main(args):
     #         model = train_neural_network_model(X_train_kf, y_train_kf, input_shape, epochs, batch_size)
     #         evaluate_neural_network(model, X_test_kf, y_test_kf) 
     
-    #     fold_no = fold_no + 1
-    
-    # Train using Neural Network
-    # input_shape = X_train.shape[1]
-    # epochs = 10
-    # batch_size = 64
-    # with mlflow.start_run(run_name=get_friendly_run_id("NeuralNetwork")):
-    #     model = train_neural_network_model(X_train, y_train, input_shape, epochs, batch_size)
-    #     evaluate_neural_network(model, X_test, y_test)
-        
+    #     fold_no = fold_no + 1    
     
 def train_logistic_regression_model(X_train, y_train, reg_rate, solver):
     print("Traning LogisticRegression model...")
@@ -171,6 +165,17 @@ def get_friendly_run_id(model_name = None):
 def read_data(data_path):
     # Read data
     data = pd.read_csv(data_path, index_col=0)
+    return data
+
+def balance_data(data):
+    # Level label samples
+    # Pick random sample data from train data where column Response is 0
+    false_rows = data[data['Response'] == 0].sample(frac=0.14, random_state=42)
+    true_rows = data[data['Response'] == 1]
+
+    # Concatenate the false_rows and true_rows
+    data = pd.concat([false_rows, true_rows])
+    
     return data
 
 if __name__ == "__main__":
