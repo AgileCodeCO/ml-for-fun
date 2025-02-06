2fea456f4d459cbe3eff6662d15017b93df70742
Jorge Gonzalez <jorgeg@mill5.com>
2025-02-05 22:39:52 -0500

    Semantic Kernel Quick Start

---
+++ generative-ai/semantic-kernel/quick-start/SKProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.SemanticKernel;
+
+var modelId = "gpt-35-turbo-16k";
+var endpoint = "[endpoint]";  
+var apiKey = "[apikey]";
+
+var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+
+var kernel = builder.Build();
+
+var result = await kernel.InvokePromptAsync("Give me a list of breakfast foods with eggs and cheese");
+Console.WriteLine(result); 
